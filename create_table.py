3903139dc4997d1329e6c0bf788b54f104de400f
Eva Maina <evajohnson714@gmail.com>
2018-07-29 11:01:57 +0300

    Modify gitignore file

--- create_table.py
+++ create_table.py
@@ -1,19 +1,19 @@
-import sqlite3
+# import sqlite3
 
-connection = sqlite3.connect('data.db')
+# connection = sqlite3.connect('data.db')
 
-cursor = connection.cursor()
+# cursor = connection.cursor()
 
-# MUST BE INTEGER
-# This is the only place where int vs INTEGER matters—in auto-incrementing columns
-create_table = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text, password text)"
-cursor.execute(create_table)
+# # MUST BE INTEGER
+# # This is the only place where int vs INTEGER matters—in auto-incrementing columns
+# create_table = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text, password text)"
+# cursor.execute(create_table)
 
-create_table = "CREATE TABLE IF NOT EXISTS items (name text PRIMARY KEY, price real)"
-cursor.execute(create_table)
+# create_table = "CREATE TABLE IF NOT EXISTS items (name text PRIMARY KEY, price real)"
+# cursor.execute(create_table)
 
-cursor.execute("INSERT INTO items VALUES('test',12.00)")
+# cursor.execute("INSERT INTO items VALUES('test',12.00)")
 
-connection.commit()
+# connection.commit()
 
-connection.close()
+# connection.close()
